5ee5fc8a7b17ef6196fdfe199c703c4c772edd45
Eoghan O'sullivan <eoggie@pop-os.localdomain>
2020-08-13 18:51:36 +0100

    Added daily schedule

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from instapy import InstaPy
+from instapy import InstaPy, smart_run
 from details import *
+import schedule
+import time
 
-session = InstaPy(username=InstaUsername,
-                  password=InstaPassword, headless_browser=True)
-session.login()
-session.like_by_tags(["100daysofcode", "react.js",
-                      "nodejs", "pythn"], amount=5)
-session.set_relationship_bounds(enabled=True, max_followers=8500)
-session.set_do_comment(True, percentage=75)
-session.set_comments(["Love the post! ", "Fantastic", "🔥"])
-session.end()
+tags = ["100daysofcode", "react.js", "nodejs", "python",
+        "devlife", "webdevelopment", "learnCode", "pythoncode"]
+
+def dailyLikes():
+  session = InstaPy(username=InstaUsername,
+                    password=InstaPassword, headless_browser=True)
+  with smart_run(session):
+    session.set_relationship_bounds(enabled=True, max_followers=8500)
+    session.set_do_comment(True, percentage=100)
+    session.set_comments(["Love the post! ", "Fantastic", "🔥"])
+    session.set_do_follow(True, percentage=25)
+    session.like_by_tags(tags, amount=1)
+
+schedule.every().day.at("12:00").do(dailyLikes)
+
+while True:
+  schedule.run_pending()
+  time.sleep(3600)
